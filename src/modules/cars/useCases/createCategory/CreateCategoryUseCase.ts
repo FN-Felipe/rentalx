c8e46619b41d78f50aa74540ceac64ed6f79f49a
Felipe de Assis <felipeassis@hotmail.com>
2023-02-01 20:49:30 -0300

    continuacao...

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,16 +1,19 @@
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository"
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
   name: string
   description: string
 }
-
+@injectable()
 class CreateCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository) {
 
   }
-  execute({ name, description }: IRequest): void {
-    const categoryAlreadExists = this.categoriesRepository.findByName(name)
+  async execute({ name, description }: IRequest): Promise<void> {
+    const categoryAlreadExists = await this.categoriesRepository.findByName(name)
     if (categoryAlreadExists) throw new Error("Category Alread Exist!")
 
     this.categoriesRepository.create({ name, description })
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
+import "reflect-metadata"
 import express from "express"
+import './shared/container/index'
 import swaggerUi from "swagger-ui-express"
 import { router } from "./routes"
 import swaggerFile from "./swagger.json"
@@ -9,4 +11,4 @@ app.use(express.json())
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(router)
 
-app.listen(3000, () => console.log("Server is running!"))  
+app.listen(3333, () => console.log("Server is running!"))  
